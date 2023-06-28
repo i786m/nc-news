55a2129c1445a9ce7ac53443d9ff5b45f82eea9d
Imran Mohamed <info@imranbmohamed.com>
2023-06-28 14:50:18 +0100

    add GET /api/articles/:article:id endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data");
 const endpointsData = require('../endpoints.json')
+const format = require('pg-format');
 
 
 
@@ -59,4 +60,44 @@ describe('GET /api', () => {
            expect(body.endpoints).toEqual(endpointsData);
         })       
     })
+})
+
+describe('GET /api/articles/:article_id', () => { 
+    
+    test('200: should respond with an article object with the following properties author, title, article_id, body, topic, created_at, votes and article_img_url', () => {
+        return request(app)
+        .get('/api/articles/1')
+        .expect(200)
+        .then(({body})=>{
+            const expected = {
+                article_id: 1,
+                title: "Living in the shadow of a great man",
+                topic: "mitch",
+                author: "butter_bridge",
+                body: "I find this existence challenging",
+                created_at: "2020-07-09T20:11:00.000Z",
+                votes: 100,
+                article_img_url:
+                  "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+              }
+            expect(body.article).toEqual(expected);
+     })
+    })
+
+    test('400: should return a 400 error with "Bad Request" as the message if passed a malformatted id e.g a string instead of a number', () => { 
+        return request(app)
+        .get('/api/articles/bananas')
+        .expect(400)
+        .then(({ body }) => {
+          expect(body.msg).toBe('Bad Request');
+        }); })
+
+    test('404: should return a 404 error with "Not Found" as the message if passed an id that doesnt exist', () => { 
+        return request(app)
+        .get('/api/articles/55')
+        .expect(404)
+        .then(({ body }) => {
+        expect(body.msg).toBe('Not Found');
+      });
+     })
 })
--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const { getTopics } = require( './controllers/topics.controllers' );
-const { handleServerErrors } = require( './errors/errors' );
+const { handleServerErrors, handlePsqlErrors, handleCustomErrors } = require( './errors/errors' );
 const { getApiInfo } = require( './controllers/api.controllers' );
+const { getArticlesById } = require( './controllers/articles.controllers' );
 
 const app = express()
-
 
 app.get('/api', getApiInfo)
 
 app.get('/api/topics', getTopics)
+
+app.get('/api/articles/:article_id', getArticlesById)
+
+app.all('*', (_,res)=>{
+    res.status(404).send({msg: 'Not Found'})
+})
+
+app.use(handlePsqlErrors)
+
+app.use(handleCustomErrors)
 
 app.use(handleServerErrors)
 
